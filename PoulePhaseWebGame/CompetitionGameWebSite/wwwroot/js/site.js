116a7935413a82dd86ce2a4460ccb878f17cfb66
Eelco Los <5102501+EelcoLos@users.noreply.github.com>
2020-06-19 11:03:37 +0200

    Simulate only even teams only even selection of team support. Also some content regarding this program added

--- PoulePhaseWebGame/CompetitionGameWebSite/wwwroot/js/site.js
+++ PoulePhaseWebGame/CompetitionGameWebSite/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$("input[type='checkbox'].teamSelect").change(function () {
+    var a = $("input[type='checkbox'].teamSelect:checked");
+    var truefalse = a.length % 2;
+    if (truefalse == 0)
+    {
+        $("#simulateTeams").prop('disabled', false);
+    }
+    else {
+        $("#simulateTeams").prop('disabled', true);
+    }
+});
